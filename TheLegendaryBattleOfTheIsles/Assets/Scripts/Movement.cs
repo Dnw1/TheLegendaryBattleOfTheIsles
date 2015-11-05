9524855f240efc39ad407774d45a4aab3e13308c
Dylan Waij <d_way@live.nl>
2015-11-05 09:44:11 +0100

    Poc01
    -

---
+++ TheLegendaryBattleOfTheIsles/Assets/Scripts/Movement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	
+	public float Speed = 0f;
+	private float move = 0f;
+	
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+		move = Input.GetAxis ("Horizontal");
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (move * Speed, GetComponent<Rigidbody2D>().velocity.y);
+	}
+}
